475663d327a46229182f337dfa0ed7003a03f78d
luigi <luigimarencodelossantos@hotmail.com>
2022-07-21 01:43:14 -0300

    Actualizacion Luigi Marenco

--- src/Components/contenedor/conteiner.js
+++ src/Components/contenedor/conteiner.js
@@ -10,9 +10,9 @@ function Conteiner (){
     <div className='cards-container'>
     <Cards titulo='Malbec' precio='$28500' precioBotella='$4.750' imagen='Malbec'></Cards>
     <Cards titulo='Cabernet-Sauvignon' precio='$26.000' precioBotella='$4.333' imagen='Cabernet'></Cards>
-    <Cards titulo='Cabernet-Sauvignon' precio='$24.600' precioBotella='$4.100' imagen='CabernetFranc'></Cards>
-    <Cards titulo='Cabernet-Sauvignon' precio='$26.000' precioBotella='$4.166' imagen='DeSangreRedBlend'></Cards>
-    <Cards titulo='Cabernet-Sauvignon' precio='$30.000' precioBotella='$5.000' imagen='Seleccion'></Cards>
+    <Cards titulo='Cabernet-Framc' precio='$24.600' precioBotella='$4.100' imagen='CabernetFranc'></Cards>
+    <Cards titulo='De Sangre-Red Blend' precio='$26.000' precioBotella='$4.166' imagen='DeSangreRedBlend'></Cards>
+    <Cards titulo='Seleccion de la Casa' precio='$22.000' precioBotella='$5.500' imagen='Seleccion'></Cards>
     </div>
     </div>);
 }
