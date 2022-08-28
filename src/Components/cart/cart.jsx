47f992a0eadd91d6d631f24f9dda329e6b1413c7
luigi <luigimarencodelossantos@hotmail.com>
2022-08-28 14:52:47 -0300

    act item collection

--- src/Components/cart/cart.jsx
+++ src/Components/cart/cart.jsx
@@ -3,13 +3,30 @@ import { useCartContext } from "../CartContext/CartContext";
 import { Link } from 'react-router-dom';
 import ItemCart from "../ItemCart/ItemCart";
 import '../ItemCart/ItemCart.css'
-
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../firebase/Firebase";
 
 const Cart = () => {
     const {cart, precioTotal, clear} = useCartContext();
 
-    
+    const order = {
+      Comprador: {
+        nombre:'Luigi',
+        email: 'luigimarencodelossantos@hotmail.com',
+        telefono:'654321despegue',
+        direccion:'callefalsa 123'
+      },
+      items: cart.map(product => ({id: product.id,nombre: product.nombre,precio: product.precio, precioBotella: product.precioBotella, quantity: product.quantity })),
+      total: precioTotal(),
+      date: serverTimestamp()
+    }
  
+
+    const handleClick = () => {
+      const ordersCollection = collection(db, "orders");
+      addDoc(ordersCollection, order).then(({ id }) => alert("Su codigo de orden es :" +id));
+  }
+
    
     if (cart.length === 0)
     
@@ -35,7 +52,7 @@ const Cart = () => {
              <h4 className='holaDonJose'>El precio total de su compra es de ${precioTotal()}</h4> 
              
              <button onClick={()=> clear (cart)} className="btnvista" id='hola'> Limpiar Carrito Completo </button>
-             <button   className="btnvista" id='chau'>Finalizar Compra</button>
+             <button  onClick={handleClick} className="btnvista" id='chau'>Finalizar Compra</button>
             </aside>
             
             </>
